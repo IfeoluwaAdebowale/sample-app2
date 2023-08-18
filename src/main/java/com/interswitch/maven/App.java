484e92eac01db4c3246e0af27676c73334e7fa63
Ifeoluwa Adebowale <Ifeoluwa.Adebowale@Interswitchng.com>
2023-08-18 15:17:30 +0100

    Added upperLowerCase Class

--- src/main/java/com/interswitch/maven/App.java
+++ src/main/java/com/interswitch/maven/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UpperLowerCase ulCase = new UpperLowerCase();
+        System.out.printf( "%s to %sn", "Hello World!", ulCase.wordToLowerCase("Hello World") );
     }
 }
